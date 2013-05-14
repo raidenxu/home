636dcf91fffa196cd95808a4aa5e3ca6383f2983
Odin <odinmanlee@gmail.com>
2013-05-14 10:16:51 +0800

    完善

--- scripts/daemons/workers/modules/yueche.m
+++ scripts/daemons/workers/modules/yueche.m
@@ -9,7 +9,7 @@
   +----------------------------------------------------------------------+
   | Created:2013-05-09 11:43:43                              |
   +----------------------------------------------------------------------+
-  | Last-Modified:2013-05-13 18:50:50                        |
+  | Last-Modified:2013-05-14 00:00:11                        |
   +----------------------------------------------------------------------+
 */
 //$GLOBALS['cookie']='ASP.NET_SessionId=at203arvezlffwnjsbi1mxdj; CNZZDATA5234107=cnzz_eid%3D1219689921-1366350518-http%253A%252F%252F114.242.121.99%26ntime%3D1368161495%26cnzz_a%3D68%26retime%3D1368161850787%26sin%3Dnone%26ltime%3D1368161850787%26rtime%3D11; ImgCode=XOFsA6pry4k=; LoginOn=j/TwSkhTp5DQKVy172Y7C0kRc0cXtJqdva964K/LzkRIlxILwqnx9V9TEjPzWeIiGnAVVLhYECyglaEGLgSiHiOOb0x+Xy+qK+olegi4MUp0lrRRZSSUwz5rzy7MVIAl4VSag+RSAENFfUmOUnL0wt0lfCRx/Ax3cK95HWkWBgQMZSQxTPIitLHzsfMpGsL0bdpm8KJKwjF0Z5jEQVZJvNj8WZtHK5121kNKpCYUPmGLY0iPY4SJHlGz0rl682/BK2OVFJnBxfWq4MXuimrC990lfCRx/Ax3YWU1BliAnjq/SfSy9EOobLZZSf7AYbzkkhZ8eZBhcrH3tp+0bsDJxDjzqVU6HIuHCAbfUTDabzULUBbxqAQWduw+neCy0F6bZW5i9QUFhGa0DX58S4lZggo4h+aoNuJk75aV2q1fuI828EWxJu6W92zyAzU8C7/DNQcOlLGb8TiD57Ms0mP94W7vcPtRB5+pHsM0HKLBp8UYB77y9zqvzjRuypN0FwHPMEmyf1xGAx/pyeyhrs7iHgcohJ/cxaVsDjPlI4YgQFFfLJ2Jg4IDvcoMaFrc3mBxGN4t1IKXvqOyxPq8kJGpa+AcrzTGhpzg6o3lHpcwkpZoWlTbjV8nMA==';
@@ -25,3 +25,12 @@ for($i=$now;$i<=$sevenDay;$i+=86400) {
 $GLOBALS['addrs']=array(
     'odinmanlee@gmail.com',
 );
+
+$GLOBALS['runTime']=array(
+    'begin' => '0735',
+    'end' => '2000',
+);
+
+$GLOBALS['_OCR']='/opt/local/bin/gocr';
+
+$GLOBALS['_yueche']['loop']=38;
